14f1f12f7626ad2508bcab5f7ee6a5822a56ad29
Bas Pennings <baspster@gmail.com>
2021-02-24 16:43:59 +0100

    add packages and reformat docstrings

--- src/Luna.jl
+++ src/Luna.jl
@@ -28,6 +28,7 @@ export vector
 
 """
     Vector4{T} <: FieldVector{4,T}
+
 A 4-vector with elements of type `T`.
 """
 struct Vector4{T} <: FieldVector{4,T}
@@ -39,6 +40,7 @@ end
 
 """
     Matrix4x4{T} = SMatrix{4,4,T,16} where T<:Real
+
 A 4x4 matrix with elements of type `T`.
 """
 const Matrix4x4{T} = SMatrix{4,4,T,16} where T<:Real
@@ -79,13 +81,17 @@ end
 
 """
     lerp(a, b, t)
+
 Linearly interpolates between `a` and `b`.
+
 # Examples
 ```julia-repl
 julia> lerp(0, 1, 0.5)
 0.5
+
 julia> lerp(0, 2, 0.5)
 1.0
+
 julia> lerp(-2, 2, 0.9)
 1.6
 ```
@@ -94,36 +100,42 @@ lerp(a, b, t) = (1 - t) * a + t * b
 
 """
     point(x, y, z)
+
 Creates a position vector in 3-dimensional space.
 """
 point(x, y, z) = Vector4(promote(x, y, z, one(x))...)
 
 """
     vector(x, y, z)
+
 Creates a direction vector in 3-dimensional space.
 """
 vector(x, y, z) = Vector4(promote(x, y, z, zero(x))...)
 
 """
     point(u)
+
 Converts 4-vector `u` into a position vector.
 """
 point(u) = point(u.x, u.y, u.z)
 
 """
     vector(u)
+
 Converts 4-vector `u` into a direction vector.
 """
 vector(u) = vector(u.x, u.y, u.z)
 
 """
     ispoint(u)
+
 Returns `true` if 4-vector `u` is a position vector.
 """
 ispoint(u) = isone(u.w)
 
 """
     isvector(u)
+
 Returns `true` if 4-vector `u` is a direction vector.
 """
 isvector(u) = iszero(u.w)
@@ -131,7 +143,7 @@ isvector(u) = iszero(u.w)
 """
     cross(u::Vector4, v::Vector4)
 
-Compute the cross product of two 4-vectors. 
+Computes the cross product of two 4-vectors. 
 The `w` component is ignored and the result is returned as
 a new direction vector.
 
@@ -143,12 +155,14 @@ julia> u = vector(1,1,0)
  1
  0
  0
+
 julia> v = vector(0,0,1)
 4-element Vector4{Int64} with indices SOneTo(4):
  0
  0
  1
  0
+ 
 julia> cross(u, v)
 4-element Vector4{Int64} with indices SOneTo(4):
   1
@@ -209,6 +223,7 @@ _shear(xy, xz, yx, yz, zx, zy) =
 
 """
     translate(dx, dy, dz)
+
 Creates a translation operation.
 
 # Examples
@@ -225,6 +240,7 @@ translate(dx, dy, dz) = m -> m * _translate(dx, dy, dz)
 
 """
     scale(sx, sy, sz)
+
 Creates a scaling operation.
 
 # Examples
@@ -241,6 +257,7 @@ scale(sx, sy, sz) = m -> m * _scale(sx, sy, sz)
 
 """
     rotx(rad)
+
 Creates a rotation operation along the x-axis.
 
 # Examples
@@ -251,7 +268,8 @@ julia> Matrix4x4{Float64}(I) |> rotx(π)
  0.0  -1.0          -1.22465e-16  0.0
  0.0   1.22465e-16  -1.0          0.0
  0.0   0.0           0.0          1.0
-julia> Matrix4x4{Float64}(I) |> rotx(π/5)
+
+ julia> Matrix4x4{Float64}(I) |> rotx(π/5)
 4×4 StaticArrays.SArray{Tuple{4,4},Float64,2,16} with indices SOneTo(4)×SOneTo(4):
  1.0  0.0        0.0       0.0
  0.0  0.809017  -0.587785  0.0
@@ -263,21 +281,27 @@ rotx(rad) = m -> m * _rotx(rad)
 
 """
     roty(rad)
+
 Creates a rotation operation along the y-axis.
+
 See also [`rotx`](@ref) for examples. 
 """
 roty(rad) = m -> m * _roty(rad)
 
 """
     rotz(rad)
+
 Creates a rotation operation along the z-axis.
+
 See also [`rotx`](@ref) for examples. 
 """
 rotz(rad) = m -> m * _rotz(rad)
 
 """
     shear(xy, xz, yx, yz, zx, zy)
+
 Creates a shearing operation.
+
 # Examples
 ```julia-repl
 julia> Matrix4x4{Float64}(I) |> shear(1, 2, 3, -1, -2, -3)
@@ -292,11 +316,14 @@ shear(xy, xz, yx, yz, zx, zy) = m -> m * _shear(xy, xz, yx, yz, zx, zy)
 
 """
     Transform(m)
+
 Creates a transform for matrix `m`.
+
 # Remarks
 A `Transform` is basically just a memoized inversion matrix so that calling
 `inv(t::Transform)` returns the cached inversion instead of recalculating it. 
 This implies `inv(t) == inv(matrix(t))`.
+
 # Examples
 ```julia-repl
 julia> m = Matrix4x4{Float64}(I) |> scale(0.5, 0.25, 0.75)
@@ -305,8 +332,10 @@ julia> m = Matrix4x4{Float64}(I) |> scale(0.5, 0.25, 0.75)
  0.0  0.25  0.0   0.0
  0.0  0.0   0.75  0.0
  0.0  0.0   0.0   1.0
-julia> m |> Transform
+
+ julia> m |> Transform
 Transform([0.5 0.0 0.0 0.0; 0.0 0.25 0.0 0.0; 0.0 0.0 0.75 0.0; 0.0 0.0 0.0 1.0])
+
 julia> inv(t) == inv(matrix(t))
 true
 ```
@@ -319,11 +348,14 @@ end
 
 """
     matrix(t)
+
 Returns the transform matrix associated with transformation `t`.
+
 # Examples
 ```julia-repl
 julia> t = Matrix4x4{Float64}(I) |> scale(0.5, π/5, -1) |> Transform
 Transform([0.5 0.0 0.0 0.0; 0.0 0.6283185307179586 0.0 0.0; 0.0 0.0 -1.0 0.0; 0.0 0.0 0.0 1.0])
+
 julia> matrix(t)
 4×4 StaticArrays.SArray{Tuple{4,4},Float64,2,16} with indices SOneTo(4)×SOneTo(4):
  0.5  0.0        0.0  0.0
@@ -336,13 +368,16 @@ matrix(t) = t.matrix
 
 """
     inv(t::Transform)
+
 Returns the inverse transform matrix.
 """
 Base.inv(t::Transform) = t.matrix⁻¹
 
 """
     eltype(::Transform{T})
+
 Returns the element type of the matrices wrapped by a transform.
+
 # Remarks
 Transforms depend on the `inv` method for matrices which creates 
 matrices with element type `Float64` or `Float32` depending on 
@@ -353,24 +388,28 @@ Base.eltype(::Transform{T}) where T = T
 
 """
     transform(obj)
+
 Returns the transformation associated with object `obj`.
 """
 transform(obj) = obj.transform
 
 """
     sphere(t)
+
 Returns a unit sphere with transformation `t`.
 """
 sphere(t) = Object{:sphere,eltype(t)}(t)
 
 """
     cube(t)
+
 Returns a unit cube with transformation `t`.
 """
 cube(t) = Object{:cube,eltype(t)}(t)
 
 """
     Intersection(t, obj::Object{S,T}) where {S,T}
+
 Creates a new intersection.
 """
 Intersection(t, obj::Object{S,T}) where {S,T} = Intersection(convert(T, t), obj)
