e070d352df9434938e9935cc429ec548f19c55b4
Bas Pennings <baspster@gmail.com>
2023-07-02 14:44:49 +0200

    Primitive transformations

--- luna.go
+++ luna.go
@@ -4,16 +4,10 @@ import (
 	"github.com/go-gl/mathgl/mgl64"
 )
 
+type Vec2 = mgl64.Vec2
 type Vec3 = mgl64.Vec3
 type Vec4 = mgl64.Vec4
 type Mat4x4 = mgl64.Mat4
-
-var Identity = &Mat4x4{
-	1, 0, 0, 0,
-	0, 1, 0, 0,
-	0, 0, 1, 0,
-	0, 0, 0, 1,
-}
 
 func Vector(x, y, z float64) Vec4 {
 	return Vec4{x, y, z, 0}
@@ -23,9 +17,36 @@ func Point(x, y, z float64) Vec4 {
 	return Vec4{x, y, z, 1}
 }
 
+func Identity() Mat4x4 {
+	return Mat4x4{
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1,
+	}
+}
+
 func Translate(tx, ty, tz float64) Mat4x4 {
 	return mgl64.Translate3D(tx, ty, tz)
 }
+
+func Scale(sx, sy, sz float64) Mat4x4 {
+	return mgl64.Scale3D(sx, sy, sz)
+}
+
+func RotateX(angle float64) Mat4x4 {
+	return mgl64.HomogRotate3DX(angle)
+}
+
+func RotateY(angle float64) Mat4x4 {
+	return mgl64.HomogRotate3DY(angle)
+}
+
+func RotateZ(angle float64) Mat4x4 {
+	return mgl64.HomogRotate3DZ(angle)
+}
+
+// TODO: shearing
 
 /*
 type material struct {
--- luna_test.go
+++ luna_test.go
@@ -1,10 +1,17 @@
 package luna_test
 
 import (
+	"math"
 	"testing"
 
 	"github.com/basp/luna"
 )
+
+const PiOver4 = math.Pi / 4
+const PiOver2 = math.Pi / 2
+const Sqrt2Over2 = math.Sqrt2 / 2
+
+var Sqrt3Over3 = math.Sqrt(3) / 3
 
 func TestIdentity(t *testing.T) {
 	var tests = []struct {
@@ -29,7 +36,7 @@ func TestIdentity(t *testing.T) {
 		{3, 2, 0},
 		{3, 3, 1},
 	}
-	m := luna.Identity
+	m := luna.Identity()
 	for _, tt := range tests {
 		i := m.Index(tt.row, tt.col)
 		ans := m[i]
@@ -64,11 +71,80 @@ func TestPrimitiveTransformations(t *testing.T) {
 			luna.Vector(-3, 4, 5),
 			luna.Vector(-3, 4, 5),
 		},
+		{
+			"scaling matrix applied to a point",
+			luna.Scale(2, 3, 4),
+			luna.Point(-4, 6, 8),
+			luna.Point(-8, 18, 32),
+		},
+		{
+			"scaling matrix applied to a vector",
+			luna.Scale(2, 3, 4),
+			luna.Vector(-4, 6, 8),
+			luna.Vector(-8, 18, 32),
+		},
+		{
+			"multiplying by the inverse of a scaling matrix",
+			luna.Scale(2, 3, 4).Inv(),
+			luna.Vector(-4, 6, 8),
+			luna.Vector(-2, 2, 2),
+		},
+		{
+			"reflection is scaling by a negative value",
+			luna.Scale(-1, 1, 1),
+			luna.Point(2, 3, 4),
+			luna.Point(-2, 3, 4),
+		},
+		{
+			"rotating a point 45 degrees around the x axis",
+			luna.RotateX(PiOver4),
+			luna.Point(0, 1, 0),
+			luna.Point(0, Sqrt2Over2, Sqrt2Over2),
+		},
+		{
+			"rotating a point 90 degrees around the x axis",
+			luna.RotateX(PiOver2),
+			luna.Point(0, 1, 0),
+			luna.Point(0, 0, 1),
+		},
+		{
+			"the inverse of an x-rotation rotates in the opposite direction",
+			luna.RotateX(PiOver4).Inv(),
+			luna.Point(0, 1, 0),
+			luna.Point(0, Sqrt2Over2, -Sqrt2Over2),
+		},
+		{
+			"rotating a point 45 degrees around the y axis",
+			luna.RotateY(PiOver4),
+			luna.Point(0, 0, 1),
+			luna.Point(Sqrt2Over2, 0, Sqrt2Over2),
+		},
+		{
+			"rotating a point 90 degrees around the y axis",
+			luna.RotateY(PiOver2),
+			luna.Point(0, 0, 1),
+			luna.Point(1, 0, 0),
+		},
+		{
+			"rotating a point 45 degrees around the z axis",
+			luna.RotateZ(PiOver4),
+			luna.Point(0, 1, 0),
+			luna.Point(-Sqrt2Over2, Sqrt2Over2, 0),
+		},
+		{
+			"rotating a point 90 degrees around the z axis",
+			luna.RotateZ(PiOver2),
+			luna.Point(0, 1, 0),
+			luna.Point(-1, 0, 0),
+		},
 	}
+	const threshold = 0.000001
 	for _, tt := range tests {
-		ans := tt.transform.Mul4x1(tt.v)
-		if ans != tt.want {
-			t.Errorf("got %v, want %v", ans, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			ans := tt.transform.Mul4x1(tt.v)
+			if !ans.ApproxEqualThreshold(tt.want, threshold) {
+				t.Errorf("got %v, want %v", ans, tt.want)
+			}
+		})
 	}
 }
